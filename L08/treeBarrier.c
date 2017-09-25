aac7d580ce4ee10c1f64dbe4011125fbb9a09036
tcew <tim.warburton@gmail.com>
2017-09-25 17:09:07 -0400

    working version

--- L08/treeBarrier.c
+++ L08/treeBarrier.c
@@ -5,7 +5,7 @@
 
 
 void barrier(){
-
+  MPI_Status status;
   int rank, size, alive, tag = 999;
 
   int sizeBuffer = 1;
@@ -47,5 +47,57 @@ void barrier(){
     alive *= 2;
 
   }
+
+  /* reverse the phone tree */
+  rank = size-1-rank;
+  
+  /* number of "alive" processes */
+  alive = 1;
+
+  /* keep doing tree stuff until all processes are alive */
+  while(alive<size){
+
+    if(rank < alive){
+      /* sender */
+      int realDest = size-1-(rank + alive);
+      
+      /* is this a legal send */
+      if(realDest >=0 && realDest<size)
+	MPI_Send(sendBuffer, sizeBuffer, MPI_INT, realDest, tag, MPI_COMM_WORLD);
+    }
+
+    if(rank >= alive && rank < 2*alive){
+      /* receiver */
+      int realSource = size-1-(rank - alive);
+
+      /* is this a legal recv */
+      if(realSource >= 0 && realSource<size)
+	MPI_Recv(recvBuffer, sizeBuffer, MPI_INT, realSource, tag, MPI_COMM_WORLD,
+		 &status);
+    }
+
+    alive *= 2;
+
+  }
+
+  if(rank==0)
+    printf("woo hoo\n");
+}
+
+int main(int argc, char **argv){
+
+  MPI_Init(&argc, &argv);
+  
+  barrier();
+
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if(rank==0) printf("ha ha from rank 0\n");
+  
+  MPI_Finalize();
+  
+  
 
 }
